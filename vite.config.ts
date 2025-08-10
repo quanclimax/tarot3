5bbd22fe16f7d22a817ee33784a3855ac7f9e828
quanphamhoang <quanpham3012@gmail.com>
2025-08-10 15:22:13 +0700

    fix: add empty arrays for listCSS, listSyncJS, and listAsyncJS

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,13 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./",
   server: {
     host: "::",
     port: 8080,
+  },
+  build: {
+    polyfillModulePreload: false,
   },
   plugins: [
     react(),
